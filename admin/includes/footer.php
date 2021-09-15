864dc60f5a000aed635cd397dadaab296687cbce
Bhavishya <bhavi.khanchandani@gmail.com>
2021-09-15 18:20:20 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,13 @@
+ <!-- Footer Start-->
+    <div class="footer-copyright-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copy-right">
+                        <p>VESIT SETU MANAGEMENT SYSTEM</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End-->
---
+++ user/includes/dbconnection.php
@@ -0,0 +1,17 @@
+<?php 
+// DB credentials.
+define('DB_HOST','db5002273471.hosting-data.io');
+define('DB_USER','dbu660666');
+define('DB_PASS','2712001vV***');
+define('DB_NAME','dbs1830887');
+define('DB_PORT','3306');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+?>
